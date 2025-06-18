a95bad87a35dce1405e30117b475f40a23f9fd99
ryuj7338 <ryuj7338@gmail.com>
2025-06-18 22:53:32 +0900

    채용공고 a태그 추가

--- src/main/java/com/example/demo/controller/testController.java
+++ src/main/java/com/example/demo/controller/testController.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 //package com.example.demo.controller;
 //
 //import org.springframework.ui.Model;
@@ -12,18 +12,4 @@
 //        model.addAttribute("naverRedirectUri", naverApi.getNaverRedirectUri());
 //    }
 //}
-=======
-package com.example.demo.controller;
 
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-
-public class testController {
-
-    @GetMapping("/login")
-    public String loginForm(Model model) {
-        model.addAttribute("naverClientId", naverApi.getNaverCliendId());
-        model.addAttribute("naverRedirectUri", naverApi.getNaverRedirectUri());
-    }
-}
->>>>>>> 641ab7f613c18b59f6d9be0463f8d567b37cfb87
