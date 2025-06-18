641ab7f613c18b59f6d9be0463f8d567b37cfb87
ryuj7338 <ryuj7338@gmail.com>
2025-06-18 17:49:36 +0900

    네이버 api 연동중

--- src/main/java/com/example/demo/util/Ut.java
+++ src/main/java/com/example/demo/util/Ut.java
@@ -1,12 +1,68 @@
 package com.example.demo.util;
 
+import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Array;
+import java.net.URLEncoder;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Map;
 
 public class Ut {
 
-    public static boolean isEmptyOrNull(String str) {
+    public static String jsReplace(String resultCode, String msg, String replaceUri) {
 
+        if (resultCode == null) {
+            resultCode = "";
+        }
+
+        if (msg == null) {
+            msg = "";
+        }
+
+        if (replaceUri == null) {
+            replaceUri = "/";
+        }
+
+        String resultMsg = resultCode + " / " + msg;
+
+        return Ut.f("""
+				<script>
+					let resultMsg = '%s'.trim();
+
+					if(resultMsg.length > 0){
+						alert(resultMsg);
+					}
+
+					location.replace('%s');
+				</script>
+				""", resultMsg, replaceUri);
+    }
+
+    public static String jsHistoryBack(String resultCode, String msg) {
+        if (resultCode == null) {
+            resultCode = "";
+        }
+
+        if (msg == null) {
+            msg = "";
+        }
+
+        String resultMsg = resultCode + " / " + msg;
+
+        return Ut.f("""
+				<script>
+					let resultMsg = '%s'.trim();
+
+					if(resultMsg.length > 0){
+						alert(resultMsg);
+					}
+
+					history.back();
+				</script>
+				""", resultMsg);
+    }
+
+    public static boolean isEmptyOrNull(String str) {
         return str == null || str.trim().length() == 0;
     }
 
@@ -27,6 +83,7 @@ public class Ut {
         if (obj.getClass().isArray()) {
             return Array.getLength(obj) == 0;
         }
+
         return false;
     }
 
@@ -34,48 +91,33 @@ public class Ut {
         return String.format(string, args);
     }
 
-    public static String jsReplace(String resultCode, String msg, String replaceUri) {
-
-        if (resultCode == null) {
-            resultCode = "";
+    public static String getEncodedCurrentUri(String currentUri) {
+        try {
+            return URLEncoder.encode(currentUri, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return currentUri;
         }
-        if (msg == null) {
-            msg = "";
-        }
-        if (replaceUri == null) {
-            replaceUri = "/";
-        }
-
-        String resultMsg = resultCode + "/" + msg;
-
-        return Ut.f("""
-                <script> let resultMsg = '%s'.trim();
-                if(resultMsg.length > 0){
-                alert(resultMsg);
-                }
-                location.replace('%s');
-                </script>
-                """, resultMsg, replaceUri);
     }
 
-    public static String jsHistoryBack(String resultCode, String msg){
+    // sha256
+    public static String sha256(String input) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] hash = md.digest(input.getBytes("UTF-8"));
+            StringBuffer hexString = new StringBuffer();
 
-        if (resultCode == null) {
-            resultCode = "";
+            for (int i = 0; i < hash.length; i++) {
+                String hex = Integer.toHexString(0xff & hash[i]);
+                if (hex.length() == 1)
+                    hexString.append('0');
+                hexString.append(hex);
+            }
+
+            return hexString.toString();
+        } catch (NoSuchAlgorithmException | UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return null;
         }
-        if (msg == null) {
-            msg = "";
-        }
-
-        String resultMsg = resultCode + "/" + msg;
-
-        return Ut.f("""
-                <script> let resultMsg = '%s'.trim();
-                if(resultMsg.length > 0){
-                alert(resultMsg);
-                }
-                history.back();
-                </script>
-                """, resultMsg);
     }
 }
---
+++ src/main/java/com/example/demo/vo/NaverApi.java
@@ -0,0 +1,38 @@
+package com.example.demo.vo;
+
+import lombok.Getter;
+import lombok.Value;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+
+@Slf4j
+@Getter
+@Component(value = "naverApi")
+public class NaverApi {
+
+    @Value("${naver.client_id}")
+    private String naverClientId;
+    @Value("${naver.redirect_uri}")
+    private String redirect_uri;
+    @Value("${naver.client_secret}")
+    private String client_secret;
+
+    public String getAccessToken(String code, String state) {
+
+        String reqUrl = "http://nid.naver.com/oauth2.0/token";
+
+        HttpHeaders headers = new HttpHeaders();
+
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", "authorization_code");
+        params.add("code", code);
+        params.add("redirect_uri", redirect_uri);
+        params.add("client_id", naverClientId);
+        params.add("client_secret", client_secret);
+        params.add("state", state);
+    }
+}
