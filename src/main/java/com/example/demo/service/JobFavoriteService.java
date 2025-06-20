0a0311bef60956cfa7b4ceea5c5314c213748733
ryuj7338 <ryuj7338@gmail.com>
2025-06-20 09:13:45 +0900

    ã로컬ã컬 변경 사항 저장

--- src/main/java/com/example/demo/service/JobFavoriteService.java
+++ src/main/java/com/example/demo/service/JobFavoriteService.java
@@ -4,6 +4,7 @@ package com.example.demo.service;
 import com.example.demo.repository.JobFavoriteRepository;
 import com.example.demo.vo.JobPosting;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,21 +13,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class JobFavoriteService {
 
-    private JobFavoriteRepository jobFavoriteRepository;
+//    @Autowired
+//    private JobFavoriteRepository jobFavoriteRepository;
 
-    public boolean isFavorited(int memberId, int jobPostingId) {
-        return jobFavoriteRepository.countByMemberIdAndJobPostingId(memberId, jobPostingId);
-    }
-
-    public void add(int memberId, int jobPostingId) {
-        jobFavoriteRepository.insert(memberId, jobPostingId);
-    }
-
-    public void remove(int memberId, int jobPostingId) {
-        jobFavoriteRepository.delete(memberId, jobPostingId);
-    }
-
-    public List<JobPosting> getFavoriteByMemberId(int memberId) {
-        return jobFavoriteRepository.findFavoriteJobsByMemberId(memberId);
-    }
 }
